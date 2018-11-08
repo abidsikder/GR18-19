b14f85619f750198f878c6cc00e2449547fbc261
Max Popick <student@max-popick.dalton.org>
2018-11-08 11:30:42 -0500

    omni opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode_Omni.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode_Omni.java
@@ -97,10 +97,10 @@ public class TestOpMode_Omni extends OpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fl");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "fr");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bl");
+        backRightDrive = hardwareMap.get(DcMotor.class, "br");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
